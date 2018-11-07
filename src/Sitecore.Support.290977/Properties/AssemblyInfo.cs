790425455b315b0b5350b5ef48b9f33a7bb8c88a
dk\ivd <ivd@sitecore.net>
2018-11-07 17:26:28 +0800

    Empty solution

---
+++ src/Sitecore.Support.290977/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.290977")]
+[assembly: AssemblyProduct("Sitecore.Support.290977")]
+[assembly: ComVisible(false)]
